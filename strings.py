9382c226163f309f797a5b7961a533bdec4845b4
Miss Mwendwa <wanjamwesh@gmail.com>
2022-01-18 16:09:08 +0300

    New Additions

---
+++ strings.py
@@ -0,0 +1,13 @@
+# create a multistring of characters
+message = '''
+Hello Jane,
+
+Thank you for contacting us on your problem.
+We have fixed the issue. Kindly review it and get back to us.
+
+with regards,
+The Support Team
+'''
+
+print(message)
+
---
+++ type_conversion.py
@@ -0,0 +1,13 @@
+#practise on type conversions
+birth_year = input("Birth year: ")
+print(type(birth_year))
+age = 2022 - int(birth_year)
+print(type(age))
+print(age)
+
+#create a program that converts users weight in pounds to kilograms
+weight_in_pounds = input("Weight: ")
+print(type(weight_in_pounds))
+weight_in_kg = int(weight_in_pounds) * 0.454 
+print(type(weight_in_kg))
+print(weight_in_kg2)
